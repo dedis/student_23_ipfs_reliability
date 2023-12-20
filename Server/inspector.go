816426cccd831d70bf21ba7361b820a6f1f08000
Léo Theytaz <leo.theytaz@epfl.ch>
2023-12-20 18:23:50 +0100

    selectBlockHeuristic

--- Server/inspector.go
+++ Server/inspector.go
@@ -1,8 +1,11 @@
 package Server
 
 import (
+	"ipfs-alpha-entanglement-code/entangler"
 	"math/rand"
 )
+
+const MaxNeighbourTries = 4
 
 // ComputeHealth
 // @Description: Computes the estimated health of the file, corresponding to repairability of the file.
@@ -18,20 +21,92 @@ func (fs *FileStats) ComputeHealth() float32 {
 }
 
 func pickNeighbour(fs *FileStats, blockNum *int, isData bool) bool {
-	// TODO Impl
-	return false
+	var blocks map[uint]WatchedBlock
+	if isData {
+		blocks = fs.DataBlocksMissing
+	} else {
+		blocks = fs.ParityBlocksMissing
+	}
+	if len(blocks) == 0 {
+		return false
+	}
+
+	var neighbour *entangler.Block
+	found := false
+
+	for i := 0; i < MaxNeighbourTries && !found; i++ {
+		n := rand.Intn(len(blocks))
+
+		var block *entangler.Block
+		if isData {
+			block = fs.lattice.DataBlocks[n]
+		} else {
+			block = fs.lattice.ParityBlocks[fs.strandNumber][n]
+		}
+
+		n = rand.Intn(2)
+		if n == 0 { // TODO simplify if-logic
+			if len(block.RightNeighbors) == 0 {
+				if len(block.LeftNeighbors) == 0 {
+					continue
+				}
+				neighbour = block.LeftNeighbors[0]
+			} else {
+				neighbour = block.RightNeighbors[0]
+			}
+		} else {
+			if len(block.LeftNeighbors) == 0 {
+				if len(block.RightNeighbors) == 0 {
+					continue
+				}
+				neighbour = block.RightNeighbors[0]
+			}
+			neighbour = block.LeftNeighbors[0]
+		}
+		found = true
+	}
+
+	if found {
+		*blockNum = neighbour.Index
+	}
+
+	return found
 }
 
-func pickInFailedRegion(fs *FileStats, blockNum *int, isData bool) bool {
-	// TODO Impl
+func pickInFailedRegion(badRegions map[string][]string, fs *FileStats, blockNum *int, isData bool) bool {
+	var blocks map[uint]WatchedBlock
+	if isData {
+		blocks = fs.validDataBlocksHistory
+	} else {
+		blocks = fs.validParityBlocksHistory
+	}
+
+	for i, block := range blocks { // TODO map always traversed in the same order ? or can randomize
+		_, in := badRegions[block.Peer.Region]
+		if in {
+			*blockNum = int(i)
+			return true
+		}
+	}
 
 	return false
 }
 
 func pickRetry(fs *FileStats, blockNum *int, isData bool) bool {
-	// TODO Impl
+	var blocks map[uint]WatchedBlock
+	if isData {
+		blocks = fs.DataBlocksMissing
+	} else {
+		blocks = fs.ParityBlocksMissing
+	}
+	if len(blocks) == 0 {
+		return false
+	}
 
-	return false
+	n := rand.Intn(len(blocks))
+
+	*blockNum = n
+	return true
 }
 
 // selectBlockHeuristic
@@ -47,7 +122,7 @@ func (s *Server) selectBlockHeuristic(fs *FileStats) (uint, bool) {
 
 		if n < 3 && pickNeighbour(fs, &blockNumber, true) {
 			// 3/7 chance to pick a neighbour of a missing block (if exists, else fallback to others random)
-		} else if n < 5 && pickInFailedRegion(fs, &blockNumber, true) {
+		} else if n < 5 && pickInFailedRegion(s.state.potentialFailedRegions, fs, &blockNumber, true) {
 			// 2/7 chance to try using tag:region to find new missing blocks (if exists, else fallback to random)
 		} else if n < 6 && pickRetry(fs, &blockNumber, true) {
 			// 1/7 chance to retry a missing block
@@ -62,7 +137,7 @@ func (s *Server) selectBlockHeuristic(fs *FileStats) (uint, bool) {
 		n = rand.Intn(7)
 
 		if n < 3 && pickNeighbour(fs, &blockNumber, false) {
-		} else if n < 5 && pickInFailedRegion(fs, &blockNumber, false) {
+		} else if n < 5 && pickInFailedRegion(s.state.potentialFailedRegions, fs, &blockNumber, false) {
 		} else if n < 6 && pickRetry(fs, &blockNumber, false) {
 		} else {
 			blockNumber = rand.Intn(len(fs.lattice.ParityBlocks[fs.strandNumber]))
@@ -110,7 +185,7 @@ func (s *Server) InspectFile(fs *FileStats) {
 			watchedBlock.CID = blockCID
 			watchedBlock.Probability = 0.33
 
-			allocations, err := s.client.IPFSClusterConnector.GetPinAllocations(blockCID) //ips... without port
+			allocations, err := s.client.IPFSClusterConnector.GetPinAllocations(blockCID) //ips...
 			if err != nil {
 				return
 			}
@@ -123,7 +198,11 @@ func (s *Server) InspectFile(fs *FileStats) {
 				s.state.potentialFailedRegions[watchedBlock.Peer.Region] = append(s.state.potentialFailedRegions[watchedBlock.Peer.Region], watchedBlock.Peer.Name)
 			}
 
-			fs.DataBlocksMissing[blockNumber] = watchedBlock
+			if isData {
+				fs.DataBlocksMissing[blockNumber] = watchedBlock
+			} else {
+				fs.ParityBlocksMissing[blockNumber] = watchedBlock
+			}
 		}
 
 		// FIXME: Too harsh?
@@ -131,11 +210,24 @@ func (s *Server) InspectFile(fs *FileStats) {
 
 		fs.Health = fs.ComputeHealth()
 		if fs.Health < s.repairThreshold {
-			// TODO trigger repair
+			// TODO trigger repair (sample func for now)
+			s.repairFile(fs, 4, 2)
 		}
 	}
 }
 
 func (fs *FileStats) updateBlockProb(testedBlockProb float32, totalNbBlocks int) {
 	fs.EstimatedBlockProb = (fs.EstimatedBlockProb*float32(totalNbBlocks-1) + testedBlockProb) / float32(totalNbBlocks)
+}
+
+func (s *Server) repairFile(fs *FileStats, depth uint, numPeers int) {
+	op := CollaborativeRepairOperation{
+		FileCID:  fs.fileCID,
+		MetaCID:  fs.MetadataCID,
+		Depth:    depth,
+		Origin:   s.clusterIP, // TODO In the preventive repair case, there is no need to forward the result to an "origin peer". Blocks should simply be re-updated
+		NumPeers: numPeers,
+	}
+
+	s.StartCollabRepair(&op)
 }
